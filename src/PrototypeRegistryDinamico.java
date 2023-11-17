c3f228cef82d52115b26e07db3b1521813c19f9a
joao.souza <joao.paulo.de.souza15t@gmail.com>
2023-11-17 13:00:54 -0300

    Criado prototipo de registro dinamico

--- src/PrototypeRegistryDinamico.java
+++ src/PrototypeRegistryDinamico.java
@@ -3,18 +3,28 @@ import java.util.Map;
 
 public class PrototypeRegistryDinamico {
     public static void main(String[] args) throws CloneNotSupportedException {
-        ClienteRegistry2 clienteRegistry2 = ClienteRegistry2.getInstance();
-        Cliente2 cliente = clienteRegistry2.getCliente2("PESSOA FISICA");
+        criarModelo('J',"SC");
+        criarModelo('F',"RS");
+        ClienteRegistry3 clienteRegistry3 = ClienteRegistry3.getInstance();
+        Cliente3 cliente = clienteRegistry3.getCliente3("F RS");
         cliente.nomeCliente = "João Paulo de Souza";
         cliente.documento = "1214456921";
         System.out.println(cliente);
 
-        Cliente2 cliente2 = clienteRegistry2.getCliente2("PESSOA JURIDICA");
+        Cliente3 cliente2 = clienteRegistry3.getCliente3("J SC");
         cliente2.nomeCliente = "Guloseimas LTDA";
         cliente2.documento = "05.886.835/0001-04";
         cliente2.codigoCliente +=1;
         System.out.println(cliente);
         System.out.println(cliente2);
+    }
+    public static void criarModelo(char tipoPessoa,String UF){
+        Cliente3 cliente3 = new Cliente3();
+        cliente3.tipoPessoa = tipoPessoa;
+        cliente3.UF = UF;
+
+        String chave = String.format("%c %s",tipoPessoa, UF);
+        ClienteRegistry3.getInstance().addCliente3(chave,cliente3);
     }
 }
 
@@ -23,7 +33,7 @@ final class Cliente3 implements Cloneable{
     public String nomeCliente;
     public char tipoPessoa;
     public String documento;
-
+    public String  UF;
     public Cliente3(){
         this.codigoCliente += 1;
     }
@@ -35,22 +45,13 @@ final class Cliente3 implements Cloneable{
 
     @Override
     public String toString() {
-        return String.format("Nome: %s - código %d | Documento %s | Tipo de Pessoa %c",nomeCliente,codigoCliente,documento,tipoPessoa);
+        return String.format("Nome: %s - código %d | Documento %s | Tipo de Pessoa %c | Estado: %s",nomeCliente,codigoCliente,documento,tipoPessoa,UF);
     }
 }
 
 final class ClienteRegistry3{
     private static final ClienteRegistry3 INSTANCE = new ClienteRegistry3();
     private static final Map<String,Cliente3> REGISTRY = new HashMap<>();
-    static {
-        Cliente3 c2 = new Cliente3();
-        c2.tipoPessoa = 'F';
-        REGISTRY.put("PESSOA FISICA", c2);
-
-        Cliente3 c3 = new Cliente3();
-        c3.tipoPessoa = 'J';
-        REGISTRY.put("PESSOA JURIDICA",c3);
-    }
     public static ClienteRegistry3 getInstance(){
         return INSTANCE;
     }
@@ -60,5 +61,9 @@ final class ClienteRegistry3{
         } catch (CloneNotSupportedException ex) {
         }
         return null;
+    }
+
+    public Cliente3 addCliente3(String chave, Cliente3 cliente3){
+        return REGISTRY.put(chave,cliente3);
     }
 }
